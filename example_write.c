bbf4e4e074cf77671d8ef18778327f1f80e055c6
Jufo <jufo@atla.sk>
2011-11-27 07:21:13 -0600

    update Makefile

--- example_write.c
+++ example_write.c
@@ -8,14 +8,11 @@
 int main(void)
 {
 	gpio_open(157,1);
-
-	while (1) {
-		gpio_write(157,0);
-		sleep(1);
-		gpio_write(157,1);
-		sleep(1);
-	}
-
+	gpio_write(157,1);
+	sleep(1);
+	gpio_write(157,0);
+	sleep(1);
 	gpio_close(157);
+return 0;
 }
 
