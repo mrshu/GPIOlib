23f20cc9bca825965e2aeb1bc99a4d70c121a4c2
Domenico Luciani <domenico.luciani@email.it>
2013-01-18 12:58:52 +0100

    Update gpio.h

--- gpio.h
+++ gpio.h
@@ -2,13 +2,18 @@
 #define _GPIO_H
 
 #include <stdio.h>
-#include <stdlib.h>
+
+#define EXPORT "/sys/class/gpio/export"
+#define UNEXPORT "/sys/class/gpio/unexport"
+#define IN "in\n"
+#define OUT "out\n"
 
 
 int gpio_open(int port, int DDR);
 int gpio_close(int port);
 int gpio_read(int port);
 int gpio_write(int port,int value);
+int pin_exist(int port);
 
 #endif
 
