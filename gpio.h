b7341402b7a1027a5c0467f5516b8baf8f01074a
Domenico Luciani <domenico.luciani@email.it>
2013-01-18 12:26:12 +0100

    Update gpio.h
    update function prototipies.

--- gpio.h
+++ gpio.h
@@ -4,10 +4,10 @@
 #include <stdio.h>
 
 
-void gpio_open(int port, int DDR);
-void gpio_close(int port);
+int gpio_open(int port, int DDR);
+int gpio_close(int port);
 int gpio_read(int port);
-void gpio_write(int port,int value);
+int gpio_write(int port,int value);
 
 #endif
 
